fcedbbdf494862e19e629e45290c460e9f95a149
malamatayali71-a11y <malamatayali71@gmail.com>
2026-04-05 03:28:14 +0200

    Refactor config.js to update global variables

--- config.js
+++ config.js
@@ -1,30 +1,25 @@
-require('dotenv').config();
+const fs = require('fs');
+const chalk = require('chalk');
 
-global.APIs = {
-    xteam: 'https://api.xteam.xyz',
-    dzx: 'https://api.dhamzxploit.my.id',
-    lol: 'https://api.lolhuman.xyz',
-    violetics: 'https://violetics.pw',
-    neoxr: 'https://api.neoxr.my.id',
-    zenzapis: 'https://zenzapis.xyz',
-    akuari: 'https://api.akuari.my.id',
-    akuari2: 'https://apimu.my.id',
-    nrtm: 'https://fg-nrtm.ddns.net',
-    bg: 'http://bochil.ddns.net',
-    fgmods: 'https://api-fgmods.ddns.net'
-};
+global.owner = ["260773158979@s.whatsapp.net"]; // ← CHANGE THIS TO YOUR NUMBER
+global.botname = "H.A.C. System";
+global.packname = "H.A.C.";
+global.author = "To Be Hero X RP";
+global.prefix = ".";           // Original mod commands
+global.hacPrefix = "!";        // H.A.C. System commands
 
-global.APIKeys = {
-    'https://api.xteam.xyz': 'd90a9e986e18778b',
-    'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-    'https://api.neoxr.my.id': 'yourkey',
-    'https://violetics.pw': 'beta',
-    'https://zenzapis.xyz': 'yourkey',
-    'https://api-fgmods.ddns.net': 'fg-dylux'
-};
+global.sessionName = "session";
+global.ownername = "GM";
+global.ownerNumber = "260XXXXXXXXX";
 
 module.exports = {
-    WARN_COUNT: 3,
-    APIs: global.APIs,
-    APIKeys: global.APIKeys
+  owner,
+  botname,
+  packname,
+  author,
+  prefix,
+  hacPrefix,
+  sessionName,
+  ownername,
+  ownerNumber
 };
